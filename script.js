e92db8d5d25cb5e8e2aebdd4390b6b455615cbe4
kunukuntla sanjay <kunukuntlasanjay5@gmail.com>
2026-07-02 20:40:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+console.log("Welcome to Sanjay's Portfolio!");
